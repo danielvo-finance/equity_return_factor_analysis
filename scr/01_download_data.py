3de4b40672ab4dbe0bfbad61c6b1af2ed83a8bf1
danielvo-finance <danielvo.bus@gmail.com>
2026-01-11 16:08:31 -0600

    Update 01_download_data.py

--- scr/01_download_data.py
+++ scr/01_download_data.py
@@ -11,18 +11,23 @@ import os
 RAW_DIR = "data/raw"
 
 tickers = input("Enter stock ticker(s) to analyze (comma seperated): ").upper()
-tickers.split(",")
+tickers = [t.strip() for t in tickers.split(",")]
 
 start_date = input("Enter the start date of the data (e.g. 2015-01-01): ")
 end_date = input("Enter the end date: ")
 
 def download_data(ticker, start, end):
     os.makedirs(RAW_DIR, exist_ok=True)
+    path = f"{RAW_DIR}/{ticker}.csv"
 
-    # takes input from user and receive data from Yahoo Finance
-    df = yf.download(ticker, start=start, end=end, progress=False, group_by="column", auto_adjust=True)
+    if os.path.exists(path):
+        return
+    
+    # Takes input from user and download data from Yahoo Finance
+    df = yf.download(ticker, start=start, end=end, progress=False, auto_adjust=True)
 
-    df.to_csv(f"{RAW_DIR}/{ticker}.csv")
+    # Creates a csv for the downloaded data in data/raw/
+    df.to_csv(path)
 
 for t in tickers:
-    download_data(tickers, start_date, end_date)
+    download_data(t, start_date, end_date)
